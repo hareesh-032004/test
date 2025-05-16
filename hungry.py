a08fb7c2dcb529551b33a022f3ebaaefddeedb27
hareesh <hareesh03vuyyala@gmail.com>
2025-05-16 22:30:57 +0530

    Revert "burger"
    This reverts commit a5643695fa66404a2c6cbea76fe0ee541ce2469d.

--- hungry.py
+++ hungry.py
@@ -3,6 +3,5 @@ hungry = input('Are you hungry ?  ')
 if hungry == 'yes' or 'Yes' or 'Yeah':
     print('Then eat biriyani')
     print('eat pizza')
-    print('eat burger')
 else:
     print('do homework')
