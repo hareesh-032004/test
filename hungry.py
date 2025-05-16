a5643695fa66404a2c6cbea76fe0ee541ce2469d
hareesh <hareesh03vuyyala@gmail.com>
2025-05-16 22:29:52 +0530

    burger

--- hungry.py
+++ hungry.py
@@ -3,5 +3,6 @@ hungry = input('Are you hungry ?  ')
 if hungry == 'yes' or 'Yes' or 'Yeah':
     print('Then eat biriyani')
     print('eat pizza')
+    print('eat burger')
 else:
     print('do homework')
