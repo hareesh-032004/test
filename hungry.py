f46bc95fca7bab435f32663cd5c54fc9b5754024
hareesh <hareesh03vuyyala@gmail.com>
2025-05-24 18:50:50 +0530

    added yo

--- hungry.py
+++ hungry.py
@@ -1,7 +1,8 @@
 hungry = input('Are you hungry ?  ')
 
-if hungry == 'yes' or 'Yes' or 'Yeah':
+if hungry == 'yes' or 'Yes' or 'Yeah' and hungry == 'yo':
     print('Then eat biriyani')
     print('Then eat pizza')
+    print('then eat burger')
 else:
     print('do homework')
